697ead42f5b682b55babf3ebd2d18bc7cf8d1360
LesterOsana18 <lesterfosana@iskolarngbayan.pup.edu.ph>
2025-02-01 01:56:19 +0800

    Update local IP address in config.js for development

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const URL = 'http://192.168.100.195:3001'; // Update with your local IP
+// export const URL = 'http://192.168.100.195:3001'; // LOCAL IP = REIGNIEL 
+export const URL = 'http://192.168.100.242:3001'; // LOCAL IP = LESTER
